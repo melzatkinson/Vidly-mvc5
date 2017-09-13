22179a4ffbe632489c3990a3db5f42a6b7c6ccd3
melzatkinson <melzatkinson@gmail.com>
2017-09-13 20:33:43 +0200

    Added repo for online mvc 5 course (Vidly project)

---
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+  public class CustomersController : Controller
+  {
+    List<Customer> _customers = new List<Customer>
+    {
+      new Customer() {Name = "John Smith", Id = 0},
+      new Customer() {Name = "Mary Williams", Id = 1}
+    };
+
+    //-------------------------------------------------------------------------
+
+    public ActionResult Index()
+    {
+      var viewModel = new CustomerViewModel()
+      {
+        Customers = _customers
+      };
+
+      return View( viewModel );
+    }
+
+    //-------------------------------------------------------------------------
+
+    public ActionResult Details( int id )
+    {
+      return View( _customers[ id ] );
+    }
+
+    //-------------------------------------------------------------------------
+  }
+}
---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+  public class MoviesController : Controller
+  {
+    //-------------------------------------------------------------------------
+
+    List<Movie> _movies = new List<Movie>
+    {
+      new Movie() {Name = "Shrek", Id = 0},
+      new Movie() {Name = "Wall-e", Id = 1}
+    };
+
+    //-------------------------------------------------------------------------
+
+    public ActionResult Index()
+    {
+      var viewModel = new MovieViewModel
+      {
+        Movies = _movies
+      };
+
+      return View( viewModel );
+    }
+
+    //-------------------------------------------------------------------------
+
+    public ActionResult Details( int id )
+    {
+      return View( _movies[ id ] );
+    }
+
+    //-------------------------------------------------------------------------
+  }
+}
